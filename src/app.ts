0e597b1897f2ff22c7af6d4c54db78244cd6e13b
Per Lundberg <per.lundberg@ecraft.com>
2017-02-21 22:48:20 +0200

    Added login view

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
 import { remote } from 'electron';
-import { Router, RouterConfiguration } from 'aurelia-router';
+import { NavigationInstruction, Next, PipelineStep, Router, Redirect, RouterConfiguration } from 'aurelia-router';
 
 export class App {
   router: Router;
 
   configureRouter(config: RouterConfiguration, router: Router){
     config.title = 'noTime';
+    config.addAuthorizeStep(AuthorizeStep);
     config.map([
-      { route: '',           moduleId: 'pages/time_entry/time_entry', title: 'noTime' , name: 'time_entry' },
-      { route: 'login',      moduleId: 'pages/login/login',           title: 'Login', },
+      { route: '',           moduleId: 'pages/time_entry/time_entry', title: 'noTime' ,    name: 'time_entry' },
+      { route: 'login',      moduleId: 'pages/login/login',           title: 'Login',      name: 'login', settings: { authentication: false } },
       { route: 'estimation', moduleId: 'pages/estimation/estimation', title: 'Estimation', name: 'estimation' }
     ]);
 
@@ -20,5 +21,16 @@ export class App {
       message: 'test',
       buttons: ["OK"]
     });
+  }
+}
+
+class AuthorizeStep implements PipelineStep {
+  public run(navigationInstruction: NavigationInstruction, next: Next): Promise<any> {
+    // Okay, I know, I know. Authentication and authorization are two different things. But I didn't know of a better way to plug in to Aurelias pipeline to enforce authentication than this.
+    if (navigationInstruction.getAllInstructions().some(i => i.config.settings.authentication !== false)) {
+      return next.cancel(new Redirect('login'));
+    }
+
+    return next();
   }
 }
