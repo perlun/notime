3f821fb36b0428ca99f9fa06f5e7d600574db3c7
Per Lundberg <per.lundberg@ecraft.com>
2017-02-21 23:05:30 +0200

    Added note about .js file which I cannot easily convert to .ts

--- aurelia_project/electron-fix.js
+++ aurelia_project/electron-fix.js
@@ -1,8 +1,10 @@
-define("electron", ['exports'], function (exports) {
-  if (window.nodeRequire) {
-    const electron = window.nodeRequire("electron");
+// Can't easily be rewritten in TypeScript, since this file is loaded before the Aurelia build pipeline has had a change to transpile .ts files to .js.
 
-    exports["default"] = electron;
+define('electron', ['exports'], function (exports) {
+  if (window.nodeRequire) {
+    const electron = window.nodeRequire('electron');
+
+    exports['default'] = electron;
 
     for (let key in electron) {
       exports[key] = electron[key];
