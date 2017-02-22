5aff28b13e8e03b102b209e23a902b38ce748aa2
Per Lundberg <per.lundberg@ecraft.com>
2017-02-22 09:27:13 +0200

    Added templates for some basic views
    - Started using the <router-view>, so we can support basic navigation.
    - Added placeholder views for estimation and time entry.

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,18 @@ import { remote } from 'electron';
 import { Router, RouterConfiguration } from 'aurelia-router';
 
 export class App {
-  public pages = [
-    {
-      title: "page1",
-      viewModel: "./pages/example-page",
-      model: "hello"
-    },
-    {
-      title: "page2",
-      viewModel: "./pages/example-page",
-      model: "world"
-    }
-  ]
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router){
+    config.title = 'noTime';
+    config.map([
+      { route: '',           moduleId: 'pages/time_entry/time_entry', title: 'noTime' , name: 'time_entry' },
+      { route: 'login',      moduleId: 'pages/login/login',           title: 'Login', },
+      { route: 'estimation', moduleId: 'pages/estimation/estimation', title: 'Estimation', name: 'estimation' }
+    ]);
+
+    this.router = router;
+  }
 
   open() {
     remote.dialog.showMessageBox({
--- src/elements/tab-control.ts
+++
@@ -1,7 +0,0 @@
-import { customElement, bindable } from 'aurelia-framework';
-
-@customElement("tab-control")
-export class TabControlCustomElement {
-  @bindable items = [];
-  @bindable selectedIndex = 0;
-}
