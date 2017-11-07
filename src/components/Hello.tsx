d13ff8769e5e82a8b594f2c03c0d370eb2c3118c
Per Lundberg <per.lundberg@ecraft.com>
2017-11-07 15:56:10 +0200

    Fixed SASS support
    Also started working on getting OpenSans into usage, but is working poorly. Will have to do a manual workaround for now probably, it's easier than wrestling with package management.

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import './Hello.scss';
 
 export interface HelloProps { compiler: string; framework: string; }
 
@@ -6,6 +7,6 @@ export interface HelloProps { compiler: string; framework: string; }
 // State is never set so we use the '{}' type.
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}! Cool, eh?</h1>;
+        return <div className="Hello"><h1>Hello from {this.props.compiler} and {this.props.framework}! Cool, eh?</h1></div>;
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,82 @@
 module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-      filename: "bundle.js",
-      path: __dirname + "/dist"
-  },
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
 
-  resolve: {
-      // Add '.ts' and '.tsx' as resolvable extensions.
-      extensions: [".ts", ".tsx", ".js", ".json"]
-  },
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
 
-  module: {
-      rules: [
-          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-          { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+    module: {
+        rules: [
+            {
+                exclude: [
+                  /\.html$/,
+                  // We have to write /\.(js|jsx)(\?.*)?$/ rather than just /\.(js|jsx)$/
+                  // because you might change the hot reloading server from the custom one
+                  // to Webpack's built-in webpack-dev-server/client?/, which would not
+                  // get properly excluded by /\.(js|jsx)$/ because of the query string.
+                  // Webpack 2 fixes this, but for now we include this hack.
+                  // https://github.com/facebookincubator/create-react-app/issues/1713
+                  /\.(js|jsx)(\?.*)?$/,
+                  /\.(ts|tsx)(\?.*)?$/,
+                  /\.css$/,
+                  /\.json$/,
+                  /\.bmp$/,
+                  /\.gif$/,
+                  /\.jpe?g$/,
+                  /\.png$/,
+                  /\.scss$/,
+                ],
+                loader: require.resolve('file-loader'),
+                options: {
+                  name: 'static/media/[name].[hash:8].[ext]',
+                },
+            },
 
-          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-          { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-      ]
-  },
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            },
 
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-      "react": "React",
-      "react-dom": "ReactDOM"
-  },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
+
+            {
+                test: /\.scss$/,
+                use: [
+                  require.resolve('style-loader'),
+                  {
+                    loader: require.resolve('css-loader'),
+                    options: {
+                      importLoaders: 1,
+                    },
+                  },
+                  {
+                    loader: require.resolve('sass-loader'),
+                  },
+                ],
+              }
+        ]
+    },
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
 };
